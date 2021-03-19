0b2744412b4428713e74b057702c1c53aa408870
ed1868 <ruizeduardo21@gmail.com>
2021-03-19 17:52:40 -0400

    added state checked to be false at first

--- CryptoScripture/src/components/Main.js
+++ CryptoScripture/src/components/Main.js
@@ -64,7 +64,7 @@ class Main extends Component {
   // }
   constructor(props) {
     super(props);
-    this.state = { checked: true };
+    this.state = { checked: false };
     this.handleChange = this.handleChange.bind(this);
   }
 
